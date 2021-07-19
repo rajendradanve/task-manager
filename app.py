bdac46ec8437a5bd27a063d798ed49d0d5830fd6
Rajendra <rajendradanve@gmail.com>
2021-07-19 21:11:48 +0000

    login page created

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def get_tasks():
     return render_template("tasks.html", tasks=tasks)
 
 
-@app.route("/register", methods = ["GET", "POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         # check if username already exist in db
@@ -43,14 +43,29 @@ def register():
         mongo.db.users.insert_one(register)
 
         # Put the new user into session cookie
-        session["user"]=request.form.get("username").lower()
+        session["user"] = request.form.get("username").lower()
         flash("Registration Successful")
     return render_template("register.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            #ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
-    app.run(host = os.environ.get("IP"),
+    app.run(host=os.environ.get("IP"),
     port=int(os.environ.get("PORT")),
     debug=True)
 
