e2a64a70e8187c72b57da600456b59d46f42d3f6
Rajendra <rajendradanve@gmail.com>
2021-07-23 14:02:21 +0000

    add manage categories template

--- app.py
+++ app.py
@@ -143,6 +143,13 @@ def delete_task(task_id):
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("edit_task.html", task=task, categories=categories)
 
+
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
     port=int(os.environ.get("PORT")),
